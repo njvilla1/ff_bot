41feb8ac04863cc03a997840111500151cb87312
Nicholas Villa <nicholasvilla@Nicholass-MBP.home>
2018-09-06 22:21:35 -0400

    added server router for custom responses

--- web/server.py
+++ web/server.py
@@ -1,13 +1,52 @@
 import os
 import requests
 import json
+import re
 from flask import Flask, request, jsonify
 
-BOT_ID = os.environ["BOT_ID"]
-LEAGUE_ID = os.environ["LEAGUE_ID"]
+try:
+    BOT_ID = os.environ["BOT_ID"]
+    LEAGUE_ID = os.environ["LEAGUE_ID"]
+except KeyError:
+    BOT_ID = '1'
+    LEAGUE_ID = '1'
 YEAR = 2018
 
+def write_message(msg):
+    template = {
+            "bot_id": BOT_ID,
+            "text": msg,
+            "attachments": []
+            }
+    headers = {'content-type': 'application/json'}
+
+    requests.post("https://api.groupme.com/v3/bots/post", data=json.dumps(template), headers=headers)
+
+class Router( object ):
+
+    def __init__(self, routes):
+        """ 
+        routes will be in the form:
+        [ (r'<regex_str>', handler_func), ... ]
+        """
+        self.routes = [(re.compile(r), h) for r, h in routes]
+
+    def handle(self, groupme_msg):
+        msg_text = groupme_msg['text']
+        for (route, handler_func) in self.routes:
+            if bool(route.match(msg_text)):
+                response_text = handler_func(groupme_msg)
+                write_message(response_text)
+                return response_text
+
 app = Flask("test bot")
+
+def echo_handler(msg):
+    return msg['text']
+
+router = Router([
+    (r'^@bot repeat.*$', echo_handler)
+])
 
 @app.route('/receive_msg', methods=['POST'])
 def hello_world():
@@ -29,17 +68,9 @@ def hello_world():
     } 
     """
     content = request.json
-    print("New message: {}".format(content))
+    #print("New message: {}".format(content))
+    response = router.handle(content)
+    if response:
+        print('Wrote message in group: "{}"'.format(response))
 
     return 'Hello, World!'
-
-def write_message(msg):
-    # need to POST msg like :'{"bot_id":"BOT_ID","text":"This is my message"}'
-    template = {
-            "bot_id": BOT_ID,
-            "text": msg,
-            "attachments": []
-            }
-    headers = {'content-type': 'application/json'}
-
-    r = requests.post("https://api.groupme.com/v3/bots/post", data=json.dumps(template), headers=headers)
